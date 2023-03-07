cb45fbe4e9401e719606a1130da4a547cf1f0bba
Mateo Flores <dragonnegro1231@gmail.com>
2023-03-07 12:45:28 -0500

    Agregamos swagger para la documentacion

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+// Swagger
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -28,6 +33,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+    res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
